e5b046c782b7c3d97cf2e2657f956839bb0f9344
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-21 15:52:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,17 +17,3 @@ end
 
 
 
-# array = []
-#   if array = 0
-#   puts "The line is currently empty."
-#   elsif array > 0
-#   puts "The line is currently 1. Logan 2. Avi 3. Spencer"
-# end
-# end
-  
-  # else array > 0
-  # puts "Welcome #{name} you are number #{number} in line."
-   
-
-
-
