a5b09c5501ec2d7f6d7ca6f0ad42b10eac243319
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-21 15:51:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ if deli.empty?
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
-     current_line << " #{i}."
+     current_line << " #{i}. #{person}"
     end
     puts current_line
   end
