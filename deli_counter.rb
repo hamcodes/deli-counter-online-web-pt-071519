b51dc3ef92a93d49861c993a58f7a95dc1c975c3
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-21 15:43:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 def line(array)
-  if katz_deli = [] 
-  puts "The line is currently empty."
+if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+        Here push onto the current_line
+    end
+    puts current_line
+  end
 end
 end
 
